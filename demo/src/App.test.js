d367f53b14e3e65dbc67211d786a6972defd5cb8
Bergomezz <bernardogomezz@yahoo.com.br>
2023-11-08 16:27:13 -0300

    trying to configure the project

---
+++ demo/src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Enzyme from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
+import App from './App';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('renders learn react link', () => {});
